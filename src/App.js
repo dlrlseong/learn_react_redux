eba271cf92910dca60688f446422db013ab65f18
dlrlseong <leekisung0212@gmail.com>
2022-09-05 01:34:06 +0900

    256 새 슬라이스에서 읽기 및 디스패치하기

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
-import Counter from './components/Counter';
-import Header from './components/Header';
-import Auth from './components/Auth'
-
+import Counter from "./components/Counter";
+import Header from "./components/Header";
+import Auth from "./components/Auth";
+import { useSelector } from "react-redux";
+import UserProfile from "./components/UserProfile";
 
 function App() {
+  const isAuth = useSelector((state) => state.auth.isAuthenticated);
   return (
     <>
-    <Header></Header>
-    <Auth></Auth>
-    <Counter></Counter>
+      <Header></Header>
+      {!isAuth && <Auth></Auth>}
+      {isAuth && <UserProfile></UserProfile>}
+      <Counter></Counter>
     </>
   );
 }
