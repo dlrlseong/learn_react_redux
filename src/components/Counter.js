1b668c3cc3279222c0bd2c869c92d4f544bd46ad
dlrlseong <leekisung0212@gmail.com>
2022-09-05 00:54:56 +0900

    255 다중 슬라이스 작업하기

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -4,9 +4,9 @@ import { useSelector, useDispatch } from "react-redux/es/exports";
 
 const Counter = () => {
   const dispatch = useDispatch();
-  const counter = useSelector((state) => state.counter);
+  const counter = useSelector((state) => state.counter.counter);
 
-  const show = useSelector((state) => state.showCounter);
+  const show = useSelector((state) => state.counter.showCounter);
 
   const increaseHandler = () => {
     dispatch(counterActions.increase(10));
